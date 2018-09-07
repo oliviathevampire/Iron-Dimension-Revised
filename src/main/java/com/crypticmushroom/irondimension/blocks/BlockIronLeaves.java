5122e62f859e814aab81f68bcaf90b21d3e9b7d3
Andromander <cathy@feodoroff.com>
2018-09-07 20:40:24 +1000

    Fix some sounds

--- src/main/java/com/crypticmushroom/irondimension/blocks/BlockIronLeaves.java
+++ src/main/java/com/crypticmushroom/irondimension/blocks/BlockIronLeaves.java
@@ -1,6 +1,7 @@
 package com.crypticmushroom.irondimension.blocks;
 
 import com.crypticmushroom.irondimension.TabIDL;
+import com.crypticmushroom.irondimension.registry.BlocksIDL;
 import com.crypticmushroom.irondimension.registry.util.RegisterModelUtil;
 import com.google.common.collect.Lists;
 import net.minecraft.block.BlockLeaves;
@@ -8,10 +9,8 @@ import net.minecraft.block.BlockPlanks;
 import net.minecraft.block.state.BlockStateContainer;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.client.renderer.block.statemap.StateMap;
-import net.minecraft.init.Blocks;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
-import net.minecraft.util.BlockRenderLayer;
-import net.minecraft.util.EnumFacing;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.IBlockAccess;
 import net.minecraftforge.client.model.ModelLoader;
@@ -19,6 +18,7 @@ import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 
 import java.util.List;
+import java.util.Random;
 
 public class BlockIronLeaves extends BlockLeaves implements RegisterModelUtil {
 
@@ -38,12 +38,12 @@ public class BlockIronLeaves extends BlockLeaves implements RegisterModelUtil {
     public List<ItemStack> onSheared(ItemStack item, IBlockAccess world, BlockPos pos, int fortune) {
         return Lists.newArrayList(new ItemStack(this));
     }
-/*
+
     @Override
     public Item getItemDropped(IBlockState state, Random par2Random, int par3) {
         return Item.getItemFromBlock(BlocksIDL.iron_sapling);
     }
-*/
+
     @Override
     public int getSaplingDropChance(IBlockState state) {
         return 20;
@@ -72,25 +72,6 @@ public class BlockIronLeaves extends BlockLeaves implements RegisterModelUtil {
             i |= 8;
         }
         return i;
-    }
-
-    @Override
-    @Deprecated
-    public boolean isOpaqueCube(IBlockState state) {
-        return Blocks.LEAVES.isOpaqueCube(state);
-    }
-
-    @Override
-    @SideOnly(Side.CLIENT)
-    public BlockRenderLayer getRenderLayer() {
-        return Blocks.LEAVES.getRenderLayer();
-    }
-
-    @Override
-    @SideOnly(Side.CLIENT)
-    @Deprecated
-    public boolean shouldSideBeRendered(IBlockState state, IBlockAccess world, BlockPos pos, EnumFacing side) {
-        return Blocks.LEAVES.shouldSideBeRendered(state, world, pos, side);
     }
 
     @SideOnly(Side.CLIENT)
