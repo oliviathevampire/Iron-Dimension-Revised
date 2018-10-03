5bacad3a910b75b80a5aa4a285d6d83fc7230587
Andromander <cathy@feodoroff.com>
2018-10-03 22:18:25 +1000

    Tada, versioning magic

--- src/main/java/com/crypticmushroom/irondimension/IronDimension.java
+++ src/main/java/com/crypticmushroom/irondimension/IronDimension.java
@@ -27,7 +27,7 @@ import org.apache.logging.log4j.Logger;
 public class IronDimension {
     public static final String MODID = "irondimension";
     public static final String NAME = "Iron Dimension";
-    public static final String VERSION = "1.0";
+    public static final String VERSION = "${version}";
 
     public static final BlocksIDL blocks = new BlocksIDL();
     public static final ItemsIDL items = new ItemsIDL();
