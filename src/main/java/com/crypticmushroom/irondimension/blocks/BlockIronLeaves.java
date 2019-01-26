7093f8c4e1826e053b538f07d7601532d08691dc
Cat Core <arthur.koalas@free.fr>
2019-01-26 18:19:17 +0100

    19w04b

--- src/main/java/com/crypticmushroom/irondimension/blocks/BlockIronLeaves.java
+++ src/main/java/com/crypticmushroom/irondimension/blocks/BlockIronLeaves.java
@@ -29,12 +29,12 @@ public class BlockIronLeaves extends LeavesBlock {
 
     @Override
     public boolean hasRandomTicks(BlockState blockState_1) {
-        return (Integer)blockState_1.get(field_11199) == 7 && !(Boolean)blockState_1.get(field_11200);
+        return (Integer)blockState_1.get(DISTANCE) == 7 && !(Boolean)blockState_1.get(PERSISTENT);
     }
 
     @Override
-    public void randomTick(BlockState blockState_1, World world_1, BlockPos blockPos_1, Random random_1) {
-        if (!(Boolean)blockState_1.get(field_11200) && (Integer)blockState_1.get(field_11199) == 7) {
+    public void onRandomTick(BlockState blockState_1, World world_1, BlockPos blockPos_1, Random random_1) {
+        if (!(Boolean)blockState_1.get(PERSISTENT) && (Integer)blockState_1.get(DISTANCE) == 7) {
             dropStacks(blockState_1, world_1, blockPos_1);
             world_1.clearBlockState(blockPos_1);
         }
@@ -42,8 +42,8 @@ public class BlockIronLeaves extends LeavesBlock {
     }
 
     @Override
-    public void scheduledTick(BlockState blockState_1, World world_1, BlockPos blockPos_1, Random random_1) {
-        world_1.setBlockState(blockPos_1, method_10300(blockState_1, world_1, blockPos_1), 3);
+    public void onScheduledTick(BlockState blockState_1, World world_1, BlockPos blockPos_1, Random random_1) {
+        world_1.setBlockState(blockPos_1, updateDistamceFromLogs(blockState_1, world_1, blockPos_1), 3);
     }
 
     @Override
@@ -53,15 +53,15 @@ public class BlockIronLeaves extends LeavesBlock {
 
     @Override
     public BlockState getStateForNeighborUpdate(BlockState blockState_1, Direction direction_1, BlockState blockState_2, IWorld iWorld_1, BlockPos blockPos_1, BlockPos blockPos_2) {
-        int int_1 = method_10302(blockState_2) + 1;
-        if (int_1 != 1 || (Integer)blockState_1.get(field_11199) != int_1) {
+        int int_1 = getDistanceFromLog(blockState_2) + 1;
+        if (int_1 != 1 || (Integer)blockState_1.get(DISTANCE) != int_1) {
             iWorld_1.getBlockTickScheduler().schedule(blockPos_1, this, 1);
         }
 
         return blockState_1;
     }
 
-    private static BlockState method_10300(BlockState blockState_1, IWorld iWorld_1, BlockPos blockPos_1) {
+    private static BlockState updateDistamceFromLogs(BlockState blockState_1, IWorld iWorld_1, BlockPos blockPos_1) {
         int int_1 = 7;
         BlockPos.PooledMutable blockPos$PooledMutable_1 = BlockPos.PooledMutable.get();
         Throwable var5 = null;
@@ -73,7 +73,7 @@ public class BlockIronLeaves extends LeavesBlock {
             for(int var8 = 0; var8 < var7; ++var8) {
                 Direction direction_1 = var6[var8];
                 blockPos$PooledMutable_1.set((Vec3i)blockPos_1).setOffset(direction_1);
-                int_1 = Math.min(int_1, method_10302(iWorld_1.getBlockState(blockPos$PooledMutable_1)) + 1);
+                int_1 = Math.min(int_1, getDistanceFromLog(iWorld_1.getBlockState(blockPos$PooledMutable_1)) + 1);
                 if (int_1 == 1) {
                     break;
                 }
@@ -96,14 +96,14 @@ public class BlockIronLeaves extends LeavesBlock {
 
         }
 
-        return (BlockState)blockState_1.with(field_11199, int_1);
+        return (BlockState)blockState_1.with(DISTANCE, int_1);
     }
 
-    private static int method_10302(BlockState blockState_1) {
-        if (blockState_1.getBlock() instanceof BlockIronLog) {
+    private static int getDistanceFromLog(BlockState blockState_1) {
+        if (blockState_1.getBlock() instanceof LogBlock) {
             return 0;
         } else {
-            return blockState_1.getBlock() instanceof LeavesBlock ? (Integer)blockState_1.get(field_11199) : 7;
+            return blockState_1.getBlock() instanceof LeavesBlock ? (Integer)blockState_1.get(DISTANCE) : 7;
         }
     }
 
@@ -119,6 +119,7 @@ public class BlockIronLeaves extends LeavesBlock {
         }
 
     }
+
 
     @Environment(EnvType.CLIENT)
     public static void setRenderingMode(boolean boolean_1) {
@@ -142,11 +143,11 @@ public class BlockIronLeaves extends LeavesBlock {
 
     @Override
     protected void appendProperties(StateFactory.Builder<Block, BlockState> stateFactory$Builder_1) {
-        stateFactory$Builder_1.with(field_11199, field_11200);
+        stateFactory$Builder_1.with(DISTANCE, PERSISTENT);
     }
 
     @Override
     public BlockState getPlacementState(ItemPlacementContext itemPlacementContext_1) {
-        return method_10300((BlockState)this.getDefaultState().with(field_11200, true), itemPlacementContext_1.getWorld(), itemPlacementContext_1.getPos());
+        return updateDistamceFromLogs((BlockState)this.getDefaultState().with(PERSISTENT, true), itemPlacementContext_1.getWorld(), itemPlacementContext_1.getBlockPos());
     }
 }
